1f4238b01300bcd1be2338c848ff75bc04fc3c02
Dzenan Ridjanovic <dzenanr@gmail.com>
2013-01-29 11:43:59 -0500

    spiral 05: add and list components

---
+++ membership_s05/web/component/member/member_list_add.dart
@@ -0,0 +1,44 @@
+import 'dart:html';
+
+import 'package:web_ui/web_ui.dart';
+import 'package:membership/membership.dart';
+
+class MemberListAdd extends WebComponent {
+  Members members;
+
+  add() {
+    InputElement code = query("#code");
+    InputElement password = query("#password");
+    InputElement firstName = query("#firstName");
+    InputElement lastName = query("#lastName");
+    LabelElement message = query("#message");
+    var error = false;
+    message.text = '';
+    if (code.value.trim() == '') {
+      message.text = 'code is mandatory; ${message.text}';
+      error = true;
+    }
+    if (password.value.trim() == '') {
+      message.text = 'password is mandatory; ${message.text}';
+      error = true;
+    }
+    if (firstName.value.trim() == '') {
+      message.text = 'first name is mandatory; ${message.text}';
+      error = true;
+    }
+    if (lastName.value.trim() == '') {
+      message.text = 'last name is mandatory; ${message.text}';
+      error = true;
+    }
+    if (!error) {
+      var member = new Member(code.value);
+      member.password = password.value;
+      member.firstName = firstName.value;
+      member.lastName = lastName.value;
+      members.add(member);
+      members.order();
+      code.value = "";
+      password.value = "";
+    }
+  }
+}
